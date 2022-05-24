7ffaefa05dcccb18dfc92f7defa046ef8e14bba8
Dunya Mammadli <dunyamamadli@gmail.com>
2022-05-24 16:05:07 +0400

    Startup sqlserver,AddDbContext connection

--- Worldperfumluxurybackend/Worldperfumluxury/Data/AppDbContext.cs
+++ Worldperfumluxurybackend/Worldperfumluxury/Data/AppDbContext.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Worldperfumluxury.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
     }
 }
--- Worldperfumluxurybackend/Worldperfumluxury/Startup.cs
+++ Worldperfumluxurybackend/Worldperfumluxury/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Worldperfumluxury.Data;
 
 namespace Worldperfumluxury
 {
@@ -24,6 +26,13 @@ namespace Worldperfumluxury
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+
+            });
+            
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
