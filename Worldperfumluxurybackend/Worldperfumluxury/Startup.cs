f3f6000d7516013fdb330a4b42bda7bcaf30409f
Dunya Mammadli <dunyamamadli@gmail.com>
2022-05-25 10:53:37 +0400

    Slider crud checked

--- Worldperfumluxurybackend/Worldperfumluxury/Startup.cs
+++ Worldperfumluxurybackend/Worldperfumluxury/Startup.cs
@@ -58,9 +58,10 @@ namespace Worldperfumluxury
 
             app.UseEndpoints(endpoints =>
             {
-               endpoints.MapControllerRoute(
-                name: "areas",
-                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+              
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
                 endpoints.MapControllerRoute(
                 name : "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}"); 
