29fe0fe4606a6e67c6d3bb7bc9fd017190784361
Dunya Mammadli <dunyamamadli@gmail.com>
2022-05-29 00:38:07 +0400

    Create edit,create cruds for product

--- Worldperfumluxurybackend/Worldperfumluxury/Areas/AdminArea/Controllers/ProductController.cs
+++ Worldperfumluxurybackend/Worldperfumluxury/Areas/AdminArea/Controllers/ProductController.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Worldperfumluxury.Data;
 using Worldperfumluxury.Models;
 using Worldperfumluxury.Utilites.File;
 using Worldperfumluxury.Utilites.Helpers;
@@ -14,9 +17,17 @@ namespace Worldperfumluxury.Areas.AdminArea.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _env;
+        public ProductController(AppDbContext context, IWebHostEnvironment env)
         {
-            return View();
+            _context = context;
+            _env = env;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Product> products = await _context.Products.AsNoTracking().ToListAsync();
+            return View(products);
         }
 
         public IActionResult Create()
@@ -59,14 +70,69 @@ namespace Worldperfumluxury.Areas.AdminArea.Controllers
             {
                 Image = fileName,
 
+
             };
 
             await _context.Products.AddAsync(product);
-
-
-
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var product = await GetProductById(id);
+            if (product is null) return NotFound();
+            return View(product);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Product product)
+        {
+            var dbproduct = await GetProductById(id);
+            if (dbproduct == null) return NotFound();
+
+            if (ModelState["Photo"].ValidationState == ModelValidationState.Invalid) return View();
+
+            if (!product.Photo.CheckFileType("image/"))
+            {
+                ModelState.AddModelError("Photo", "Image type is wrong");
+                return View(dbproduct);
+            }
+
+            if (!product.Photo.CheckFileSize(10000))
+            {
+                ModelState.AddModelError("Photo", "Image size is wrong");
+                return View(dbproduct);
+            }
+
+            string path = Helper.GetFilePath(_env.WebRootPath, "assets/img/parfums", dbproduct.Image);
+
+            Helper.DeleteFile(path);
+
+
+            string fileName = Guid.NewGuid().ToString() + "_" + product.Photo.FileName;
+
+            string newPath = Helper.GetFilePath(_env.WebRootPath, "assets/img/slider", fileName);
+
+            using (FileStream stream = new FileStream(newPath, FileMode.Create))
+            {
+                await product.Photo.CopyToAsync(stream);
+            }
+
+            dbproduct.Image = fileName;
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        //Helper Method
+        private async Task<Product> GetProductById(int id)
+        {
+            return await _context.Products.FindAsync(id);
         }
     }
 }
