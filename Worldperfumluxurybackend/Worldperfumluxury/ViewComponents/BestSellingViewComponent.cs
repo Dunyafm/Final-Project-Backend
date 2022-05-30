b82a1b6ca10641bea9172efe49b4abc7b28117d9
Dunya Mammadli <dunyamamadli@gmail.com>
2022-05-30 18:54:21 +0400

    Slider,BestSellingVComponents create default

---
+++ Worldperfumluxurybackend/Worldperfumluxury/ViewComponents/BestSellingViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Worldperfumluxury.Data;
+using Worldperfumluxury.Models;
+
+namespace Worldperfumluxury.ViewComponents
+{
+    public class BestSellingViewComponent : ViewComponent
+    {
+
+        private readonly AppDbContext _context;
+        public BestSellingViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<BestSelling> bestSellings = await _context.BestSellings.ToListAsync();
+            return (await Task.FromResult(View(bestSellings)));
+        }
+
+    }
+}
---
+++ Worldperfumluxurybackend/Worldperfumluxury/ViewComponents/SliderViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Worldperfumluxury.Data;
+using Worldperfumluxury.Models;
+
+namespace Worldperfumluxury.ViewComponents
+{
+    public class SliderViewComponent : ViewComponent
+    {
+
+        private readonly AppDbContext _context;
+        public SliderViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<Slider> sliders = await _context.Sliders.ToListAsync();
+            return (await Task.FromResult(View(sliders)));
+        }
+
+
+    }
+}
