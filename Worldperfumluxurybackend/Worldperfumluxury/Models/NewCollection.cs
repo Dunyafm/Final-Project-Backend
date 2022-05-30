61c5653d1ddb9528a195e573f52e9661b7dd82a0
Dunya Mammadli <dunyamamadli@gmail.com>
2022-05-30 17:17:36 +0400

    NewCollectionVComponent finished

--- Worldperfumluxurybackend/Worldperfumluxury/Models/NewCollection.cs
+++ Worldperfumluxurybackend/Worldperfumluxury/Models/NewCollection.cs
@@ -11,13 +11,13 @@ namespace Worldperfumluxury.Models
     public class NewCollection
     {
         public int Id { get; set; }
+        public string  Name { get; set; }
         public string Header { get; set; }
         public string Desc { get; set; }
         public string Image  { get; set; }
         [NotMapped]
         [Required]
         public IFormFile Photo { get; set; }
-        public  NewCollection newCollection { get; set; }
 
     }
 }
