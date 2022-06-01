fc3e4f85e34931cd3990f909c134c69152decad2
Dunya Mammadli <dunyamamadli@gmail.com>
2022-06-01 18:19:06 +0400

    Contact page index

--- Worldperfumluxurybackend/Worldperfumluxury/Areas/AdminArea/Controllers/ProductController.cs
+++ Worldperfumluxurybackend/Worldperfumluxury/Areas/AdminArea/Controllers/ProductController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Worldperfumluxury.Controllers
+namespace Worldperfumluxury.Areas.AdminArea.Controllers
 {
     public class ProductController : Controller
     {
@@ -12,6 +12,5 @@ namespace Worldperfumluxury.Controllers
         {
             return View();
         }
-
     }
 }
